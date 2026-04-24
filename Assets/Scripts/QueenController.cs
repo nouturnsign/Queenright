9f9d5800432e3ccd5dff1ce48d95e2b7a66e5a55
Eric Wang <89937277+nouturnsign@users.noreply.github.com>
2026-04-23 23:05:53 -0700

    Add basic queen ant

---
+++ Assets/Scripts/QueenController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+[RequireComponent(typeof(Animator))]
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class QueenController : MonoBehaviour
+{
+    // Unity animation and sprites
+    private static readonly int IsWalkingHash = Animator.StringToHash("isWalking");
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+
+    // Unity inputs
+    private InputAction moveAction;
+
+    private Vector2 moveValue;
+
+    // Unity physics
+    private Rigidbody2D rb;
+
+    // custom logic
+    [SerializeField] private float moveSpeed = 0f;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        moveAction = InputSystem.actions.FindAction("Move", true);
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveValue = moveAction.ReadValue<Vector2>();   
+        bool isWalking = Mathf.Abs(moveValue.x) > 0.01f;
+        if (moveValue.x > 0)
+            spriteRenderer.flipX = true;
+        else if (moveValue.x < 0)
+            spriteRenderer.flipX = false;
+        animator.SetBool(IsWalkingHash, isWalking);
+    }
+
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveValue.x * moveSpeed, rb.linearVelocity.y);
+        
+    }
+}
